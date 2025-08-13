a042d8c18ac0587c7c6e61253f2e087b859d6ad3
Yuri Sobitnyuk <aruytehno@yandex.ru>
2025-08-13 22:50:06 +0300

    Тестовое добавление для модификации Features 2 Section для стиральной машинки

---
+++ assets/js/washing-machine.js
@@ -0,0 +1,64 @@
+(function(){
+  const root = document.querySelector('.lavanda-wm');
+  const washingMachine = root.querySelector('.lavanda-wm__machine');
+  const screen = root.querySelector('.lavanda-wm__controls');
+
+  const status = {
+    opening: {
+      isActive: true,
+      statusClass: 'isOpen',
+      controller: root.querySelector('.lavanda-wm__opening'),
+      controllerLabel: ["CLOSE", "OPEN"]
+    },
+    content: {
+      isActive: true,
+      statusClass:'isFilled',
+      controller: root.querySelector('.lavanda-wm__content'),
+      controllerLabel: ["EMPTY", "FILL"]
+    },
+    power: {
+      isActive: false,
+      statusClass: 'isWashing',
+      controller: root.querySelector('.lavanda-wm__power'),
+      controllerLabel: ["STOP", "START"]
+    }
+  };
+
+  const washSpeed = 600;
+
+  for (let action in status) {
+    const { statusClass, controller, controllerLabel } = status[action];
+
+    controller.addEventListener('click', function() {
+      const { isActive } = status[action];
+      washingMachine.classList.toggle(statusClass);
+      this.innerHTML = controllerLabel[isActive*1];
+
+      if(action === "power" && !isActive) {
+        washingMachine.classList.add(statusClass);
+        washingMachine.classList.add("isStarting");
+        setTimeout(() => { washingMachine.classList.remove("isStarting"); }, washSpeed * 2);
+      }
+
+      status[action].isActive = !isActive;
+      setTimeout(updateMachine, 100);
+    });
+  }
+
+  function updateMachine() {
+    const { opening, content, power } = status;
+    opening.controller.disabled = power.isActive;
+    content.controller.disabled = !opening.isActive;
+    power.controller.disabled = opening.isActive || !content.isActive;
+
+    if(power.isActive) {
+      screen.innerHTML = "💦"
+    } else if(!content.isActive) {
+      screen.innerHTML = "EMPTY";
+    } else if (opening.isActive) {
+      screen.innerHTML = "🙃";
+    } else {
+      screen.innerHTML = "READY";
+    }
+  }
+})();
